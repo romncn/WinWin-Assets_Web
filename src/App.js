3c03b1189e72db1ce31849c2c6629bab88484b1f
panupongjkn <panupong.jkn@gmail.com>
2020-08-12 20:17:22 +0700

    create layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Layout from "./components/components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/about">
-            {/* <About /> */}
-            <div>About</div>
-          </Route>
-          <Route path="/users">
-            {/* <Users /> */}
-            <div>Users</div>
-          </Route>
-          <Route path="/">
-            <div>Home</div>
-          </Route>
-        </Switch>
-      </Router>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route path="/about">
+              {/* <About /> */}
+              <div>About</div>
+            </Route>
+            <Route path="/users">
+              {/* <Users /> */}
+              <div>Users</div>
+            </Route>
+            <Route path="/">
+              <div>Home</div>
+            </Route>
+          </Switch>
+        </Router>
+      </Layout>
     </div>
   );
 }
