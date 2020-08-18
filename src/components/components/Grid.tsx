5bc2273de5654abab8c2a97232fe03768ac76ebf
panupongjkn <panupong.jkn@gmail.com>
2020-08-18 22:28:26 +0700

    create container component

---
+++ src/components/components/Grid.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  @media screen and (min-width: 992px) {
+    padding-left: 200px;
+    padding-right: 200px;
+  }
+`;
