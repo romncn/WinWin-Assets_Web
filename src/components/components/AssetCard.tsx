218bb120b05eabcc4af7fe43f5e183e0b84aa0c5
panupongjkn <panupong.jkn@gmail.com>
2020-08-18 23:49:14 +0700

    create asset card

---
+++ src/components/components/AssetCard.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { Card, Row, Col } from "antd";
+import { styled } from "../../style/Theme";
+
+const CardAsset = styled(Card)`
+  min-height: 374px;
+  cursor: pointer;
+  border-radius: 8px;
+  box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.14), 0px 3px 14px rgba(0, 0, 0, 0.12);
+  .ant-card-body {
+    padding: 10px;
+  }
+  .ant-card-cover img {
+    border-radius: 8px 8px 0px 0px;
+  }
+`;
+
+const ImageCover = styled.img`
+  width: 100%;
+  height: 270px;
+  object-fit: cover;
+`;
+
+const CardHover = styled.div`
+  position: absolute;
+  z-index: 2;
+  top: 0px;
+  width: 100%;
+  min-height: 374px;
+  padding: 0px 10px 0px 10px;
+  border-radius: 8px;
+  cursor: pointer;
+  opacity: 0;
+  background-color: ${(props) => props.theme.color.basecolor};
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+type AssetsProps = {
+  asset: {
+    name: string;
+    type: Array<string>;
+    location: string;
+    detail: Array<{
+      name: string;
+      size: number;
+    }>;
+  };
+};
+
+const AssetCard: React.FunctionComponent<AssetsProps> = ({ asset }) => {
+  return (
+    <div>
+      <CardAsset
+        cover={
+          <ImageCover
+            alt="example"
+            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+          />
+        }
+      >
+        <div>
+          <h3>{asset.name}</h3>
+          <h5>
+            <span>ประเภท : </span>
+            {asset.type.map((type, key) => {
+              return (
+                <span>
+                  {type}
+                  {key === asset.type.length - 1 ? "" : ", "}
+                </span>
+              );
+            })}
+          </h5>
+        </div>
+      </CardAsset>
+      <CardHover>
+        <Row align="middle" style={{ height: "374px" }}>
+          <Col>
+            <h3 style={{ color: "white" }}>{asset.name}</h3>
+            <h5 style={{ color: "white" }}>
+              <span>ประเภท : </span>
+              {asset.type.map((type, key) => {
+                return (
+                  <span>
+                    {type}
+                    {key === asset.type.length - 1 ? "" : ", "}
+                  </span>
+                );
+              })}
+            </h5>
+            <img src="/img/arrownext.png" />
+          </Col>
+        </Row>
+      </CardHover>
+    </div>
+  );
+};
+
+export default AssetCard;
