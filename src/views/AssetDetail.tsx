c9901d5d529effc1028f967c870bac0bead3bdba
panupongjkn <panupong.jkn@gmail.com>
2020-08-18 08:22:37 +0700

    update use theme color in asset detail page

--- src/views/AssetDetail.tsx
+++ src/views/AssetDetail.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { styled } from "../style/Theme";
+
+const Text = styled.span`
+  color: ${(props) => props.theme.color.base};
+`;
+
+const TextSub = styled.span`
+  color: ${(props) => props.theme.color.sub};
+`;
 
 const AssetDetail: React.SFC = (props) => {
   let { name } = useParams();
   //   console.log("name", (name+"").replace("_", " "));
-  return <div>Asset Detail</div>;
+  return (
+    <>
+      <Text>Asset Detail</Text>
+      <TextSub>SSSSS</TextSub>
+    </>
+  );
 };
 
 export default AssetDetail;
