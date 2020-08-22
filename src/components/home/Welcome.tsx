353a2ca994199b672a32c9e1aa98cf9384a1bdbb
panupongjkn <panupong.jkn@gmail.com>
2020-08-22 15:28:35 +0700

    [+] Update welcome component

--- src/components/home/Welcome.tsx
+++ src/components/home/Welcome.tsx
@@ -1,11 +1,16 @@
-import React from "react"
+import React from "react";
+import Button from "../components/Button";
 
 const Welcome: React.FC = () => {
-    return (
-        <div>
-            Welcome
-        </div>
-    )
-}
+  return (
+    <div
+      style={{ textAlign: "center", paddingTop: "50px", paddingBottom: "50px" }}
+    >
+      <h4>ยินดีต้อนรับเข้าสู่</h4>
+      <h1>WIN WIN ASSETS</h1>
+      <Button type="outline">ทำความรู้จักกับ WINWIN</Button>
+    </div>
+  );
+};
 
 export default Welcome;
