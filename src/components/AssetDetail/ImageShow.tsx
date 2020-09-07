701660f8c2e9c6276bd007e1e61f95db0c1c0130
panupongjkn <panupong.jkn@gmail.com>
2020-09-07 22:15:08 +0700

    [+] update modal

--- src/components/AssetDetail/ImageShow.tsx
+++ src/components/AssetDetail/ImageShow.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import styled from "styled-components";
+import { Modal } from "antd";
 import { Container } from "../components/Grid";
 import { Row, Col } from "antd";
 
@@ -8,6 +9,7 @@ const ImageCover = styled.img`
   height: 300px;
   border-radius: 8px;
   object-fit: cover;
+  cursor: pointer;
   @media screen and (min-width: 992px) {
     height: 350px;
   }
@@ -19,24 +21,51 @@ const SubImageCover = styled(ImageCover)`
     height: 220px;
   }
 `;
+
 type AssetImageProps = {
   image: Array<string>;
 };
 
 const ImageShow: FunctionComponent<AssetImageProps> = ({ image }) => {
+
+  const [visible, setVisible] = useState<boolean>(false);
+  const [imageModal, setImage] = useState<string>("")
+
+  const showModal = (image : string) => {
+    setImage(image)
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setVisible(false);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
   return (
     <Container>
       <Row>
-        <ImageCover src={`/img/Assets/${image[0]}`} />
+        <ImageCover onClick={() => showModal(image[0])} src={`/img/Assets/${image[0]}`} />
       </Row>
       <Row gutter={6} style={{ paddingTop: "6px" }}>
         <Col span={12}>
-          <SubImageCover src={`/img/Assets/${image[1]}`} />
+          <SubImageCover onClick={() => showModal(image[1])} src={`/img/Assets/${image[1]}`} />
         </Col>
         <Col span={12}>
-          <SubImageCover src={`/img/Assets/${image[2]}`} />
+          <SubImageCover onClick={() => showModal(image[1])} src={`/img/Assets/${image[2]}`} />
         </Col>
       </Row>
+      <Modal
+        title="รูปภาพโครงการ"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[]}
+      >
+        <img src={`/img/Assets/${imageModal}`} width="100%" />
+      </Modal>
     </Container>
   );
 };
